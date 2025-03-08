bf00fc8481a93a0cc1c3fc5201a1fa6113fbcb14
Mattia Sinisi <mattia@snisni.it>
2025-03-08 12:09:06 +0100

    Initial setup

---
+++ pages/game.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        swag
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+        <div class="flex flex-col items-center gap-2">
+            <h1 class="text-4xl">Munchkin Tracker</h1>
+
+            <NuxtLink
+                to="/game"
+                class="
+                    inline-block
+                    bg-black
+                    text-white
+                    py-2 px-4
+                    text-center
+                    rounded-lg
+                    shadow-lg
+                "
+                text="Start"
+            />
+        </div>
+    </div>
+</template>
